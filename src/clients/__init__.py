3394d34e8d49e147b6145092a930c197ca15b3ac
George Mendonça <george.henrique.mendonca@gmail.com>
2026-02-15 02:21:40 -0300

    Create __init__.py file to import clients

---
+++ src/clients/__init__.py
@@ -0,0 +1,4 @@
+from .mongodb_client import MongoDBClient
+from .mysql_client import MySQLClient
+from .kafka_client_producer import KafkaClientProducer
+from .kafka_client_consumer import KafkaClientConsumer
