73f655fc85a38fe8560ab468f0319cc11fed1836
George Mendonça <george.henrique.mendonca@gmail.com>
2026-02-15 02:43:23 -0300

    Complete MongoDB CRUD implementation

--- src/clients/mongodb_client.py
+++ src/clients/mongodb_client.py
@@ -1,34 +1,28 @@
-# mongo_client.py
+# MongoDB CRUD Implementation
 
-"""
-Este arquivo contém a implementação de um cliente MongoDB em Python.
-O cliente se conecta a um banco de dados MongoDB e fornece
-métodos para interagir com a coleção.
+from pymongo import MongoClient
 
-Exemplo de uso:
+class MongoDBClient:
+    def __init__(self, uri):
+        self.client = MongoClient(uri)
+        self.db = self.client.get_database()
 
-    from mongo_client import MongoClient
-    client = MongoClient('mongodb://localhost:27017/')
-    db = client['meu_banco']
-    collection = db['minha_colecao']
-    client.insert_document({'chave': 'valor'})
-"""
+    def create(self, collection_name, data):
+        collection = self.db[collection_name]
+        result = collection.insert_one(data)
+        return result.inserted_id
 
-from pymongo import MongoClient as PyMongoClient
+    def read(self, collection_name, query):
+        collection = self.db[collection_name]
+        result = collection.find_one(query)
+        return result
 
-class MongoClient:
-    def __init__(self, uri, db_name):
-        self.client = PyMongoClient(uri)
-        self.db = self.client[db_name]
+    def update(self, collection_name, query, update_data):
+        collection = self.db[collection_name]
+        result = collection.update_one(query, {'$set': update_data})
+        return result.modified_count
 
-    def insert_document(self, document):
-        """Insere um documento na coleção padrão."""
-        self.db.minha_colecao.insert_one(document)
-
-    def find_document(self, query):
-        """Encontra um documento com base na consulta especificada."""
-        return self.db.minha_colecao.find_one(query)
-
-    def close(self):
-        """Fecha a conexão com o banco de dados."""
-        self.client.close()
+    def delete(self, collection_name, query):
+        collection = self.db[collection_name]
+        result = collection.delete_one(query)
+        return result.deleted_count
